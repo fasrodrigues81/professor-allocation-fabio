e2485a8639fad42f8ed34a9647bc7b09af267d68
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-02-18 19:53:47 -0300

    add class

---
+++ src/main/java/com/project/professor/allocation/fabio/entity/Course.java
@@ -0,0 +1,5 @@
+package com.project.professor.allocation.fabio.entity;
+
+public class Course {
+
+}
---
+++ src/main/java/com/project/professor/allocation/fabio/entity/Departament.java
@@ -0,0 +1,21 @@
+package com.project.professor.allocation.fabio.entity;
+
+public class Departament {
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private Long id;
+	private String name;
+}
+
+
