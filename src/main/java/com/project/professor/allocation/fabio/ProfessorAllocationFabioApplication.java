ad2d44105a63e01f818e51eff41de361ee4f693c
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-02-18 14:54:16 -0300

    adicionando projeto inicial

---
+++ src/main/java/com/project/professor/allocation/fabio/ProfessorAllocationFabioApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.fabio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationFabioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationFabioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/fabio/ProfessorAllocationFabioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.fabio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationFabioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
