e0cdc6a64aacd45b4c2949236722cc4f731c908b
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-02-19 20:50:22 -0300

    gerando ID da tabela

--- src/main/java/com/project/professor/allocation/fabio/entity/Departament.java
+++ src/main/java/com/project/professor/allocation/fabio/entity/Departament.java
@@ -1,7 +1,15 @@
 package com.project.professor.allocation.fabio.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Departament {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
---
+++ src/main/java/com/project/professor/allocation/fabio/entity/entity.java
@@ -0,0 +1,5 @@
+package com.project.professor.allocation.fabio.entity;
+
+public @interface entity {
+
+}
