a549e10e746f96fc468e00e30465aad3aac34794
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-02-19 22:34:10 -0300

    mudança tabela curso e professor

--- src/main/java/com/project/professor/allocation/fabio/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/fabio/entity/Professor.java
@@ -1,5 +1,6 @@
 package com.project.professor.allocation.fabio.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,34 +8,43 @@ import javax.persistence.Id;
 
 @Entity
 public class Professor {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(length = 11, unique = true, nullable = false)
 	private String cpf;
 	private Long departmentId;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public Long getDepartmentId() {
 		return departmentId;
 	}
+
 	public void setDepartmentId(Long departmentId) {
 		this.departmentId = departmentId;
 	}
