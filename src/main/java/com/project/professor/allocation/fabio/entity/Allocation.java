2f408ecf4b3074ee33a71c2cd169eca752a449ca
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-02-19 22:47:15 -0300

    incluindo tempotal e enumerated

--- src/main/java/com/project/professor/allocation/fabio/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/fabio/entity/Allocation.java
@@ -1,57 +1,78 @@
 package com.project.professor.allocation.fabio.entity;
+
 import java.time.DayOfWeek;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Allocation {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Enumerated(EnumType.STRING)
 	private DayOfWeek day;
+	@Temporal(TemporalType.TIME)
 	private Date start;
+	@Temporal(TemporalType.TIME)
 	private Date end;
 	private Long courseId;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public DayOfWeek getDay() {
 		return day;
 	}
+
 	public void setDay(DayOfWeek day) {
 		this.day = day;
 	}
+
 	public Date getStart() {
 		return start;
 	}
+
 	public void setStart(Date start) {
 		this.start = start;
 	}
+
 	public Date getEnd() {
 		return end;
 	}
+
 	public void setEnd(Date end) {
 		this.end = end;
 	}
+
 	public Long getCourseId() {
 		return courseId;
 	}
+
 	public void setCourseId(Long courseId) {
 		this.courseId = courseId;
 	}
+
 	public Long getProfessorId() {
 		return professorId;
 	}
+
 	public void setProfessorId(Long professorId) {
 		this.professorId = professorId;
 	}
+
 	private Long professorId;
 
 }
